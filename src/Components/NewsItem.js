9711a77a6316d417f2085a1a3651778fac3851ec
prajwal wadhai <prajwalwadhai55@gmail.com>
2022-07-23 17:43:17 +0530

    Setup of NewsMonkey is done and all component will created sucessfully

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>this is item of news</div>
+    )
+  }
+}
+
+export default NewsItems
