7c3aea545ddbacffea0a31c1b60d60a0ef0eb995
prajwal wadhai <prajwalwadhai55@gmail.com>
2022-10-04 23:28:04 +0530

    video 30 completed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,11 +13,33 @@ export class Navbar extends Component {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <a className="nav-link" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/about">About</a>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Business</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Entertainment</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">General</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Health</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Science</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Sports</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">Technology</a>
+                        </li>
+                        
                         {/* <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+    static defaultProps = {
+      country: "in",
+      pageSize : 11,
+      category: 'general'
+    }
+    static propTypes={
+      country: PropTypes.string,
+      pageSize: PropTypes.number,
+      category: PropTypes.string
+    }
     articles = []
     constructor(){
         super();
@@ -15,7 +26,7 @@ export class News extends Component {
     }
 
     async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=1&pageSize=${this.props.pageSize}`;
         this.setState({loading : true});
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -27,7 +38,7 @@ export class News extends Component {
     }
 
     handlePrevClick = async ()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({loading : true});
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -41,7 +52,7 @@ export class News extends Component {
     }
     handleNextClick = async ()=>{
         if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-            let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c7501a3ee13240e7bca0c34a5d72974a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({loading : true});
             let data = await fetch(url);
             let parsedData = await data.json();
