165208baa8b93d9ef8a0262196b1c8425a064c61
prajwal wadhai <prajwalwadhai55@gmail.com>
2022-12-28 00:22:53 +0530

    Updates

---
+++ precache-manifest.3a02dfde4482fed35f11def1649bd5b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc9e8216f37fec8ee5bd",
+    "url": "/my-app/static/css/main.40d2b012.chunk.css"
+  },
+  {
+    "revision": "bc9e8216f37fec8ee5bd",
+    "url": "/my-app/static/js/main.bc9e8216.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/my-app/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "f79f8f76d2124c5a10c8",
+    "url": "/my-app/static/js/2.f79f8f76.chunk.js"
+  },
+  {
+    "revision": "5191f1b0783c9396f10c",
+    "url": "/my-app/static/js/runtime~main.5191f1b0.js"
+  },
+  {
+    "revision": "dafa4436fac90d439006f6080703fcd8",
+    "url": "/my-app/static/media/loading.dafa4436.gif"
+  },
+  {
+    "revision": "4737dc55dc388d79fb307269b131bddc",
+    "url": "/my-app/index.html"
+  }
+];
