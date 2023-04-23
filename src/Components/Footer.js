84cee71d77333143331011e01b8c1ee601535655
prajwal wadhai <prajwalwadhai55@gmail.com>
2023-04-23 13:23:18 +0530

    Carousel section done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,11 +8,11 @@ export default function Footer() {
       <div className="col-6 col-md-2 mb-3">
         <h5>Section</h5>
         <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">FAQs</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">About</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">FAQs</a></li>
         </ul>
       </div>
 
@@ -20,9 +20,9 @@ export default function Footer() {
         <h5>Section</h5>
         <ul className="nav flex-column">
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Pricing</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Features</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">About</a></li>
         </ul>
       </div>
@@ -30,11 +30,11 @@ export default function Footer() {
       <div className="col-6 col-md-2 mb-3">
         <h5>Section</h5>
         <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">FAQs</a></li>
           <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">About</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-muted">Home</a></li>
         </ul>
       </div>
 
@@ -52,7 +52,7 @@ export default function Footer() {
     </div>
 
     <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-      <p>© 2022 Company, Inc. All rights reserved.</p>
+      <p>© 2023 Company, Inc. All rights reserved.</p>
       <ul className="list-unstyled d-flex">
         {/* <li className="ms-3"><a className="link-dark" href="/"><svg className="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
         <li className="ms-3"><a className="link-dark" href="/"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -52,10 +52,10 @@ const Navbar= ()=>{
                             </ul>
                         </li> */}
                         </ul>
-                        <form className="d-flex" role="search">
+                        {/* <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                         <button className="btn btn-outline-light" type="submit">Search</button>
-                        </form>
+                        </form> */}
                     </div>
                 </div>
         </nav>
